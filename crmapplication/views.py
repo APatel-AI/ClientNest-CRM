361d4e79226d0395ec3f2818ac55dbda3076f253
APatel-AI <arpan.p.se@gmail.com>
2024-01-18 09:10:23 -0600

    add: user registration function and updated bootstrap css

--- crmapplication/views.py
+++ crmapplication/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login, logout
 #toast messages
 from django.contrib import messages
 
+from .forms import SignUpForm
+
 def home(request):
     #logging in check
     if request.method == 'POST':
@@ -12,7 +14,7 @@ def home(request):
         username = request.POST['username']
         password = request.POST['password']
         #AUTHENTICATE
-        user = authenticate(request, username = username, password=password)
+        user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
             messages.success(request, "You Have Been Logged In !")
@@ -33,3 +35,21 @@ def logout_user(request):
    logout(request)
    messages.success(request, "You have been logged out!")
    return redirect ('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authenticate and log user in if form is valid
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password = password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcom to ClientNest")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
