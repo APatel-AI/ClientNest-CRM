a918f5b2b7016e8f11661f82154f4898aaa8c247
APatel-AI <arpan.p.se@gmail.com>
2024-01-20 20:00:13 -0600

    updated home page css

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-49^enedu_bwanq3(@_zxw))ett%8@ih@cbwq6=g@bl=3d6u!w6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
